74ebd373bcb89107c242f4ebbd45923d5c822d79
guilherme.ribeiro <guilhermeribeirog70@gmail.com>
2023-12-06 11:12:38 -0400

    Atualização com a correção de bugs

--- src/Compra.java
+++ src/Compra.java
@@ -21,5 +21,8 @@ public class Compra implements Comparable<Compra>{
                 " valor =" + valor;
     }
 
-
+    @Override
+    public int compareTo(Compra outraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(outraCompra.valor));
+    }
 }
